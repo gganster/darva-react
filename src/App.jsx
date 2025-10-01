932205e731006e29fe2d484c77d3e1a7a1362a69
gganster <guilian.ganster@gmail.com>
2025-10-01 09:12:41 +0200

    use navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import {useState, useEffect} from "react";
 import AddTicketForm from "./components/AddTicketForm";
+import { useNavigate } from "react-router";
 import axios from "axios";
 
 function App() {
+  const navigate = useNavigate();
   const [loading, setLoading] = useState(true)
   const [tickets, setTickets] = useState([]);
 
   const onSubmit = async (ticket) => {
     const res = await axios.post("http://localhost:3000/tickets", ticket);
     setTickets([...tickets, res.data]);
+    navigate(`/tickets/${res.data.id}`);
   }
 
   useEffect(() => {
@@ -28,6 +31,7 @@ function App() {
         <div key={i.id} style={{border: "1px solid black", margin: 5, padding: 5}}>
           <h3>{i.title}</h3>
           <h6>{i.message}</h6>
+          <Link to={`/tickets/${i.id}`}>View</Link>
         </div>
       ))}
     </>
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <BrowserRouter>
       <Routes>
         <Route element={<Main />}>
+          <Route path="/tickets/:ticketId" element={<Tickets />} />
           <Route exact path="/" element={<App />} />
           <Route path="*" element={<p>Not found</p>} />
         </Route>
